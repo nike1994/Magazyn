f845341eb7a4b1b163130d402995629f0e63c268
Wiktoria Kozak <kozak-wiktoria@wp.pl>
2020-11-13 21:08:23 +0100

    DataBase

--- src/main/java/pl/edu/wszib/magazyn/App.java
+++ src/main/java/pl/edu/wszib/magazyn/App.java
@@ -1,16 +1,15 @@
 package pl.edu.wszib.magazyn;
 
-import io.jsondb.JsonDBTemplate;
-import pl.edu.wszib.magazyn.database.DBConfiguration;
 import pl.edu.wszib.magazyn.database.DataBase;
-import pl.edu.wszib.magazyn.database.ProductInstance;
-
-import java.security.GeneralSecurityException;
 
 public class App {
-    public static void main(String[] args) throws GeneralSecurityException {
+    public static void main(String[] args) {
 
         DataBase db = new DataBase();
+        System.out.println(db.selectAll().get(0).getQuantity());
         db.add("3039532524879", 20);
+        System.out.println(db.selectAll().get(0).getQuantity());
+        db.remove("3039532524879", 5);
+        System.out.println(db.selectAll().get(0).getQuantity());
     }
 }
